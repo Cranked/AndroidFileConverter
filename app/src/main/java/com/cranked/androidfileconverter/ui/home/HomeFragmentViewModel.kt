39459092285a7ad4f48d0178f395fc75e80bcba1
ALİ RIZA ÇELİK <riza-celik@outlook.com>
2022-07-02 13:18:46 +0300

    gitflow-feature-stash: home-fragment
    ı have editied home fragment components

---
+++ app/src/main/java/com/cranked/androidfileconverter/ui/home/HomeFragmentViewModel.kt
@@ -0,0 +1,9 @@
+package com.cranked.androidfileconverter.ui.home
+
+import androidx.lifecycle.MutableLiveData
+import com.cranked.androidcorelibrary.viewmodel.BaseViewModel
+
+class HomeFragmentViewModel : BaseViewModel() {
+    private val test = MutableLiveData<String>()
+
+}
--- app/src/main/java/com/cranked/androidfileconverter/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/cranked/androidfileconverter/ui/splash/SplashActivity.kt
@@ -1,46 +1,42 @@
 package com.cranked.androidfileconverter.ui.splash
 
-import android.animation.Animator
 import android.animation.AnimatorSet
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
+import com.cranked.androidcorelibrary.ui.raw.RawActivity
 import com.cranked.androidfileconverter.R
 import com.cranked.androidfileconverter.ui.main.MainActivity
 import com.cranked.androidfileconverter.utils.AnimationX
-import com.cranked.androidfileconverter.utils.AnimationXUtils
-import com.cranked.androidfileconverter.utils.animation.animationStart
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : RawActivity() {
     var animatorSet: AnimatorSet = AnimationX().getNewAnimatorSet()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val imageView = findViewById<ImageView>(R.id.imageView)
-
-        val pulseAnimation = object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator?) = Unit
-            override fun onAnimationEnd(animation: Animator?) {
-                imageView.clearAnimation()
-                startActivity(Intent(baseContext, MainActivity::class.java))
-                finish()
-            }
-            override fun onAnimationCancel(animation: Animator?) = Unit
-            override fun onAnimationRepeat(animation: Animator?) = Unit
-        }
-        val zoomInRightAnimation = object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator?) = Unit
-            override fun onAnimationEnd(animation: Animator?) {
-                val animatorSetX =
-                    AnimationXUtils.pulse(imageView, AnimationX().getNewAnimatorSet())
-                imageView.animationStart(2000, animatorSetX, pulseAnimation)
-            }
-            override fun onAnimationCancel(animation: Animator?) = Unit
-            override fun onAnimationRepeat(animation: Animator?) = Unit
-        }
-        val animatorSetX =
-            AnimationXUtils.zoomInRight(imageView, animatorSet)
-        imageView.animationStart(1000, animatorSetX, zoomInRightAnimation)
+//        val imageView = findViewById<ImageView>(R.id.imageView)
+//
+//        val pulseAnimation = object : Animator.AnimatorListener {
+//            override fun onAnimationStart(animation: Animator?) = Unit
+//            override fun onAnimationEnd(animation: Animator?) {
+//                imageView.clearAnimation()
+//                startActivity(Intent(baseContext, MainActivity::class.java))
+//                finish()
+//            }
+//            override fun onAnimationCancel(animation: Animator?) = Unit
+//            override fun onAnimationRepeat(animation: Animator?) = Unit
+//        }
+//        val zoomInRightAnimation = object : Animator.AnimatorListener {
+//            override fun onAnimationStart(animation: Animator?) = Unit
+//            override fun onAnimationEnd(animation: Animator?) {
+//                val animatorSetX =
+//                    AnimationXUtils.pulse(imageView, AnimationX().getNewAnimatorSet())
+//                imageView.animationStart(2000, animatorSetX, pulseAnimation)
+//            }
+//            override fun onAnimationCancel(animation: Animator?) = Unit
+//            override fun onAnimationRepeat(animation: Animator?) = Unit
+//        }
+//        val animatorSetX =
+//            AnimationXUtils.zoomInRight(imageView, animatorSet)
+//        imageView.animationStart(1000, animatorSetX, zoomInRightAnimation)
+        startActivity(MainActivity::class.java, true)
     }
 }
