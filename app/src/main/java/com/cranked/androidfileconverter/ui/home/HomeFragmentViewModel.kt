a88cea5595ecd0ef4123ece5bd54a4b11e776443
ALİ RIZA ÇELİK <riza-celik@outlook.com>
2022-07-05 16:52:56 +0300

    gitflow-feature-stash: home-fragment-menu

--- app/src/main/java/com/cranked/androidfileconverter/ui/home/HomeFragmentViewModel.kt
+++ app/src/main/java/com/cranked/androidfileconverter/ui/home/HomeFragmentViewModel.kt
@@ -1,13 +1,15 @@
 package com.cranked.androidfileconverter.ui.home
 
+import android.content.Context
+import com.cranked.androidcorelibrary.utility.FileUtils
 import com.cranked.androidcorelibrary.viewmodel.BaseViewModel
 import com.cranked.androidfileconverter.data.database.dao.ProcessedFilesDao
-import com.cranked.androidfileconverter.data.database.dao.ScannedFilesDao
-import com.cranked.androidfileconverter.data.database.entity.ProcessedFile
 import com.cranked.androidfileconverter.utils.file.FileUtility
 import javax.inject.Inject
 
-class HomeFragmentViewModel @Inject constructor(val processedFilesDao: ProcessedFilesDao) :
+class HomeFragmentViewModel @Inject constructor(val processedFilesDao: ProcessedFilesDao,val mContext: Context) :
     BaseViewModel() {
+    val sdCardState = FileUtils.isSdCardMounted(mContext)
     val storageModel = FileUtility.getFileSize()
+
 }
--- app/src/main/java/com/cranked/androidfileconverter/utils/file/FileUtility.kt
+++ app/src/main/java/com/cranked/androidfileconverter/utils/file/FileUtility.kt
@@ -6,18 +6,21 @@ import com.cranked.androidfileconverter.ui.home.StorageModel
 
 object FileUtility {
     fun getFileSize(): StorageModel {
-        val validtypes= arrayListOf("pdf","xlsx","docx","png","jpeg")
+        val validtypes = arrayListOf("pdf", "xlsx", "docx", "png", "jpeg")
         val base_path = Environment.getExternalStorageDirectory().absolutePath + "/"
         val downloads_path = base_path + Environment.DIRECTORY_DOWNLOADS
         val sdcard_path = System.getenv("EXTERNAL_STORAGE")
-        val internalStorageSize = FileUtils.getFolderFiles(base_path, 1, 1).filter { it.isDirectory or validtypes.contains(FileUtils.getExtension(it.name)) }.size.toString()
-        val downloadSize = FileUtils.getFolderFiles(downloads_path, 1, 1).filter { it.isDirectory or validtypes.contains(FileUtils.getExtension(it.name)) }.size.toString()
+        val internalStorageSize = FileUtils.getFolderFiles(base_path, 1, 1)
+            .filter { it.isDirectory or validtypes.contains(FileUtils.getExtension(it.name)) }.size.toString()
+        val downloadSize = FileUtils.getFolderFiles(downloads_path, 1, 1)
+            .filter { it.isDirectory or validtypes.contains(FileUtils.getExtension(it.name)) }.size.toString()
         val sdCardSize = FileUtils.getFolderFiles(
             sdcard_path,
             1,
             1
-        ).filter { it.isDirectory or validtypes.contains(FileUtils.getExtension(it.name)) }.size.toString()
-        val processedSize = "41"
+        )
+            .filter { it.isDirectory or validtypes.contains(FileUtils.getExtension(it.name)) }.size.toString()
+        val processedSize = "0"
         return StorageModel(
             internalStorageSize,
             sdCardSize,
